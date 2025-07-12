f69b37d550fd43c82d4b98f67019b1c237047d73
Pravinpatidar45 <136345806+Pravinpatidar45@users.noreply.github.com>
2025-07-12 17:09:26 +0530

    forget auth

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { addCategory, addServices, deleteCategory, deleteServiceforCategory, updateCategory, updateServicePrice, viewCategories, viewServicesforCategory } from "../controllers/category.controller.js";
+import { tokenVerification } from "../middleware/tokenverification.js";
 const router=express.Router();
-router.post("/category",addCategory);
-router.get("/categories",viewCategories);
-router.put("/category/:categoryId",updateCategory);
-router.delete("/category/:categoryId",deleteCategory);
-router.post("/category/:categoryId/service",addServices);
-router.get("/category/:categoryId/services",viewServicesforCategory)
-router.delete("/category/:categoryId/service/:serviceId",deleteServiceforCategory)
-router.put("/category/:categoryId/service/:serviceId",updateServicePrice)
+router.post("/category",tokenVerification(),addCategory);
+router.get("/categories",tokenVerification(),viewCategories);
+router.put("/category/:categoryId",tokenVerification(),updateCategory);
+router.delete("/category/:categoryId",tokenVerification(),deleteCategory);
+router.post("/category/:categoryId/service",tokenVerification(),addServices);
+router.get("/category/:categoryId/services",tokenVerification(),viewServicesforCategory)
+router.delete("/category/:categoryId/service/:serviceId",tokenVerification(),deleteServiceforCategory)
+router.put("/category/:categoryId/service/:serviceId",tokenVerification(),updateServicePrice)
 export default router;
